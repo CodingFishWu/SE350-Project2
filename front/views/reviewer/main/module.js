5d2c3511218c9d7e86f3c94364cd32194f0d9ade
CodingFish <CodingFishWu.gmail.com>
2016-05-04 21:08:02 +0800

    去除了审阅者界面有关模态框的js class语法

--- front/views/reviewer/main/module.js
+++ front/views/reviewer/main/module.js
@@ -186,4 +186,90 @@ class ReviewerReviewCtrl {
 
 angular.module('reviewerMainModule', [])
 .controller('reviewerMainCtrl', ['$state','$uibModal', 'PaperService', 'ExamineService', 'KeyService', ReviewerMainCtrl])
-.controller('reviewerReviewCtrl', ['$state', '$uibModalInstance', 'PaperService', 'ExamineService', 'examine', 'keys', ReviewerReviewCtrl]);
+.controller('reviewerReviewCtrl', function($state, $uibModalInstance, PaperService, ExamineService, examine, keys) {
+	let self = this
+	self.examine = examine
+	self.keys = keys
+
+	self.paper = self.examine.paper;
+		
+	self.url = 'http://202.120.40.73:28080/file/Ua46d59e19268fe/PaperServ/Paper/'+this.paper.id;
+	self.items1 = ["Strong Accept","Accept","Weak Accept","Borderline Paper","Weak Reject","Reject","Strong Reject"];
+	self.items2 = ["expert","high","medium","low","null"];
+
+	self.opinion = self.items1[0];
+	self.confidence = self.items2[0];
+
+	self.submit = function() {
+		if (!self.checkValid()) {
+			alert('输入不合法')
+			return
+		}
+
+		// 更新examine状态和信息
+		let examine = new ExamineService({
+			id: self.examine.id,
+			opinion: self.opinion,
+			confidence: self.confidence,
+			remark: self.remark,
+			status: 'finished',
+			reviewer: {
+				id: self.examine.reviewer.id,
+				type: 'User'
+			},
+			paper: {
+				id: self.paper.id,
+				type: 'Paper'
+			}
+		})
+		examine.$put().$promise
+		.then((result)=>{
+			return ExamineService.query({
+				'Examine.paper.id': self.paper.id,
+				'Examine.status': 'finished'
+			}).$promise
+		})
+		.then((result)=>{
+			if (result.Examine.length >= 3) {
+				// 需要更新paper的状态为judging
+				self.PaperService.get({id: self.paper.id}).$promise
+				.then((result)=>{
+					self.paper.userId = result.user.id
+					let paper = new PaperService({
+						id: self.paper.id,
+						title: self.paper.title,
+						author: self.paper.author,
+						correspondingauthor: self.paper.correspondingauthor,
+						affiliation: self.paper.affiliation,
+						correspondingaddress: self.paper.correspondingaddress,
+						abstraction: self.paper.abstraction,
+						createdtime: self.paper.createdtime,
+						status: 'judging',
+						serialnumber: self.paper.serialnumber,
+						deadline: self.paper.deadline,
+						user: {
+							type: 'User',
+							id: self.paper.userId
+						}
+					})
+					return paper.$put().$promise
+				})
+				.then((result)=>{
+					alert('已提交审核')
+					self.$uibModalInstance.close()
+				})
+			}
+			else {
+				alert('已提交审核')
+				self.$uibModalInstance.close()
+			}
+		})
+	}
+
+	self.checkValid = function() {
+		if (!self.remark) {
+			return false
+		}
+		return true
+	}
+});
